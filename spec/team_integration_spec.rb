a2e79db9438f1520bf9e0612684a908ef8cfe253
Brandon Seroyer <brandonseroyer@gmail.com>
2015-10-02 12:02:53 -0700

    added and passed integration spec

--- spec/team_integration_spec.rb
+++ spec/team_integration_spec.rb
@@ -1,14 +1,14 @@
-# require('capybara/rspec')
-# require('./app')
-# Capybara.app = Sinatra::Application
-# set(:show_exceptions, false)
-#
-# describe('show the add team path', {:type => :feature}) do
-#   it('adds a word to a list') do
-#     visit('/')
-#     click_link('Add New Team')
-#     fill_in('Team Name', :with => 'Blazers')
-#     click_button('Submit')
-#     expect(page).to have_content('Blazers')
-#   end
-# end
+require('capybara/rspec')
+require('./app')
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+
+describe('show the add team path', {:type => :feature}) do
+  it('adds a word to a list') do
+    visit('/')
+    click_link('Add New Team')
+    fill_in('Team Name', :with => 'Blazers')
+    click_button('Submit')
+    expect(page).to have_content('Blazers')
+  end
+end
